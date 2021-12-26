b14ab9d40351ae36d7772b557aa454eafea96d8c
justindu1017 <34846425+justindu1017@users.noreply.github.com>
2021-12-27 03:39:11 +0800

    test

--- app.py
+++ app.py
@@ -1,4 +1,27 @@
+from asyncio.runners import run
 from flask import Flask, render_template, request, redirect
+from databases import Database
+from dotenv import load_dotenv
+import os
+import asyncio
+
+
+database = ""
+
+
+async def dbConnect():
+    load_dotenv()
+    global database
+    database = Database(os.getenv("Database_Route"))
+    print("DB is ", database)
+    await database.connect()
+
+
+async def queryInsert(values):
+    query = "INSERT INTO meters(ip, port) VALUES (:ip, :port)"
+    print("here is ", database)
+    await database.execute(query=query, values=values)
+
 
 app = Flask(__name__)
 
@@ -10,10 +33,20 @@ def index():
 
 @app.route("/post", methods=['POST'])
 def getPost():
-    a = request.form
-    print("get!!!    ", a)
+    form = request.form
+    if form["selection"] == "UseIP":
+        ip = form["ip1"]+"."+form["ip2"]+"."+form["ip3"]+"."+form["ip4"]
+        port = form["port"]
+        values = {"ip": ip, "port": port}
+    asyncio.run(queryInsert(values))
+    print("OK")
     return redirect("/")
 
 
-if __name__ == '__main__':
+def main():
+    asyncio.run(dbConnect())
     app.run()
+
+
+if __name__ == '__main__':
+    main()
